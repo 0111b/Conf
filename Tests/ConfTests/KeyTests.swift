fb71a901db7e47556b1b156171fa361710c6da83
Alexandr Goncharov <adanfermer@gmail.com>
2020-06-09 17:43:57 +0300

    Fix Lint issues

--- Tests/ConfTests/KeyTests.swift
+++ Tests/ConfTests/KeyTests.swift
@@ -3,19 +3,19 @@ import XCTest
 
 final class KeyTests: XCTestCase {
     func testCreation() {
-        let k1: Key = Key()
-        XCTAssertEqual(k1.fragmets, [])
-        let k2: Key = "24"
-        XCTAssertEqual(k2.fragmets, ["24"])
-        let k3: Key = 99
-        XCTAssertEqual(k3.fragmets, ["99"])
-        let k4: Key = Key(23.4)
-        XCTAssertEqual(k4.fragmets, ["23.4"])
-        let k5: Key = Key("some")
-        XCTAssertEqual(k5.fragmets, ["some"])
-        let k6: Key = ["24", 72, 23.4, true]
-        XCTAssertEqual(k6.fragmets, ["24", "72", "23.4", "true"])
-        let k7: Key = Key([1,2,3])
-        XCTAssertEqual(k7.fragmets, ["1", "2", "3"])
+        let key1: Key = Key()
+        XCTAssertEqual(key1.fragmets, [])
+        let key2: Key = "24"
+        XCTAssertEqual(key2.fragmets, ["24"])
+        let key3: Key = 99
+        XCTAssertEqual(key3.fragmets, ["99"])
+        let key4: Key = Key(23.4)
+        XCTAssertEqual(key4.fragmets, ["23.4"])
+        let key5: Key = Key("some")
+        XCTAssertEqual(key5.fragmets, ["some"])
+        let key6: Key = ["24", 72, 23.4, true]
+        XCTAssertEqual(key6.fragmets, ["24", "72", "23.4", "true"])
+        let key7: Key = Key([1, 2, 3])
+        XCTAssertEqual(key7.fragmets, ["1", "2", "3"])
     }
 }
