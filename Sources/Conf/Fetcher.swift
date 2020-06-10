350f7b0704aac067f049d7194add605d0cac433d
Alexandr Goncharov <adanfermer@gmail.com>
2020-06-10 03:16:15 +0300

    implement basic fetchers

---
+++ Sources/Conf/Fetcher.swift
@@ -0,0 +1,19 @@
+import Foundation
+
+/// Namespace for the predefined fetchers
+enum Fetcher { }
+
+extension Fetcher {
+    
+    static let direct: (Data) -> CommonConfigurationProvider.Fetcher = { data in
+       return { data }
+    }
+
+    static let file: (String) -> CommonConfigurationProvider.Fetcher = { configName in
+        return {
+            let url = URL(fileURLWithPath: configName, isDirectory: false)
+            print(url)
+            return try Data(contentsOf: url)
+        }
+    }
+}
